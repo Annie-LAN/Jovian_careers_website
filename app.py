de49e2ce6fe0e4058d535eafe7e8cc9488ca0b35
Annie-LAN <106366246+Annie-LAN@users.noreply.github.com>
2023-11-19 04:25:22 +0000

    use Boostrap and add dynamic data using Jinja template

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Beijing, China',
+    'salary': '￥100,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Shanghai, China',
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Chicago, USA',
+    'salary': '$130,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Chicago, USA',
+    'salary': '$120,000'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, companyname="Jovian")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
